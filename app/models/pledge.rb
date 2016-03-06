ccad3bf9394c0fe4c8b1fb2e10515061ba4b9d9e
Esther Zinn <Bionicfemme@me.com>
2016-03-06 22:01:55 +0000

    Updated project and pledge models

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,9 +1,30 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :reward
-  
+
   before_validation     :generate_uuid!, :on => :create
   validates_presence_of :name, :address, :city, :country, :postal_code, :amount, :user_id
+  after_create          :check_if_funded
+
+  def charged?
+    status == "charged"
+  end
+
+  def failed?
+    status == "failed"
+  end
+
+  def pending?
+    status == "pending"
+  end
+
+  def charged!
+    update(status: "charged")
+  end
+
+  def void!
+    update(status: "void")
+  end
 
   private
 
@@ -12,4 +33,9 @@ class Pledge < ActiveRecord::Base
       self.uuid = SecureRandom.hex(16)      
     end while Pledge.find_by(:uuid => self.uuid).present?
   end
+
+  def check_if_funded
+    project.funded! if project.total_backed_amount >= project.goal
+  end
+  
 end
