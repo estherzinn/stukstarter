b14890286cd145c012b7976e2faaf0088fbe31d7
Esther Zinn <Bionicfemme@me.com>
2016-03-06 21:58:01 +0000

    changes to payments and pledges

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :projects do 
     resources :rewards, only: [:new, :create, :edit, :update, :destroy]
     resources :pledges
+    resources :payments, only: [:new, :create]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
