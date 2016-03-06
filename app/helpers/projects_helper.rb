a09a3ce687ef3c820341839df71138b0749bb205
Esther Zinn <Bionicfemme@me.com>
2016-03-06 22:41:44 +0000

    Cleaned up project views

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,19 +1,16 @@
-# == Schema Information
-#
-# Table name: projects
-#
-#  id                :integer          not null, primary key
-#  user_id           :integer
-#  name              :string
-#  short_description :text
-#  description       :text
-#  image_url         :string
-#  status            :string           default("pending")
-#  goal              :decimal(8, 2)
-#  expiration_date   :datetime
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#
-
 module ProjectsHelper
+
+  def funding_status_message(project)
+    case project.status
+      when "pending"
+        "This project will only be funded if, at least, #{number_to_currency(project.goal, unit: '$')} is pledged by #{project.expiration_date}."
+      when "funded"
+        "This project will be funded on #{project.expiration_date}."
+      when "expired"
+        "This project failed to meet the goal."
+      when "canceled"
+        "This project has been canceled."
+    end              
+  end
+
 end
